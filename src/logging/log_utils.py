acd43a2ee419886ed0d59ce850d7adc4a355f688
Arman Niaruhi <arman.niaruhi@gmail.com>
2025-09-04 23:10:50 +0200

    add data extractor

---
+++ src/logging/log_utils.py
@@ -0,0 +1,19 @@
+import logging
+from functools import wraps
+
+# Logger configuration
+logging.basicConfig(
+    level=logging.INFO,  # Must be INFO or lower
+    format="%(asctime)s | %(levelname)s | %(funcName)s | %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
+logger = logging.getLogger(__name__)
+
+def log_function(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        logger.info(f"Entering {func.__name__}")
+        result = func(*args, **kwargs)
+        logger.info(f"Exiting {func.__name__}")
+        return result
+    return wrapper
