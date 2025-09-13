08bf6e771efd60780c68754cc9177caf8dcab893
Arman Niaruhi <arman.niaruhi@gmail.com>
2025-09-13 15:24:19 +0200

    fix: fix the problem in main during import

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # main.py
 from src.pipeline.extractor import DataExtractor
 from src.pipeline.transformer import DataTransformer
-from pipeline.visualizer import DataVisulizer
+from src.pipeline.visualizer import DataVisulizer
 
 
 def main():
@@ -16,15 +16,11 @@ def main():
     # -----------------------------
     transformer = DataTransformer(**dfs)
     transformer.check_quality()
-
-    failed_indices = [1, 48, 166]
-    transformer.delete_failed_experiment(failed_experiment=failed_indices)
-
+    transformer.delete_failed_experiment(failed_experiment=[1, 48, 166])
     # Extract all tranformed sensor_data
     df_machine_and_movement, df_sensor, df_machine, df_movements = (
         transformer.get_process_data()
     )
-
     # Extract all tranformed geometry_data
     df_arc, df_lin1, df_lin2, linear_df, all_geometry_data = (
         transformer.get_geometry_data()
