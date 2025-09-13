386f475672c496e9ad4658cf0439da1633b2afe9
Arman Niaruhi <arman.niaruhi@gmail.com>
2025-09-13 15:14:59 +0200

    build: Add new extractor feature as a part of ETL Process for data engineering

--- src/pipeline/extractor.py
+++ src/pipeline/extractor.py
@@ -1,32 +1,87 @@
 import pickle
+import pandas as pd
+
 from src.logging.log_utils import log_function
-from src.utils.utils import take_all_bending_setups
 
 
-class data_extractor():
+class DataExtractor:
     def __init__(self) -> None:
         with open("data/experiments_process_and_results.pkl", "rb") as f:
             self.loaded_dict = pickle.load(f)
-         
-    @log_function   
+
+    def _take_bending_setups(
+        self, experiments_process_and_results, machine_part
+    ) -> pd.DataFrame:
+        """
+        Extracts all bending setups to a Pandas DataFrame and adds an 'Experiment_ID' column
+        if not already present.
+        """
+        if machine_part != "bending_setups":
+            df_list = []
+            for (
+                experiment_name,
+                experiment_data,
+            ) in experiments_process_and_results.items():
+                df = experiment_data[machine_part].copy()
+                # Always move or insert Experiment_ID as the first column
+                if "Experiment_ID" in df.columns:
+                    cols = ["Experiment_ID"] + [
+                        col for col in df.columns if col != "Experiment_ID"
+                    ]
+                    df = df[cols]
+                else:
+                    # Insert new column at position 0
+                    df.insert(0, "Experiment_ID", experiment_name)
+
+                df_list.append(df)
+
+            result = pd.concat(df_list, ignore_index=True)
+            result["Experiment_ID"] = result["Experiment_ID"].str.replace("Exp_", "")
+            return result
+
+        result = pd.concat(
+            [
+                experiments_process_and_results[experiment_name]["bending_setups"]
+                for experiment_name in experiments_process_and_results.keys()
+            ],
+            ignore_index=True,
+        )
+        result.columns = result.columns.str.replace("Experiment", "Experiment_ID")
+        return result
+
     def load_dict_getter(self):
         """Getter for self.load_dict
-            obj.load_dict_getter """  
+        obj.load_dict_getter"""
         return self.loaded_dict
-    
+
     @log_function
-    def get_all_bending_setups(self, machine_part):
+    def get_part_bending_setups(self, machine_part):
         """Return all bending setups from the loaded dictionary."""
-        return take_all_bending_setups(self.loaded_dict, machine_part)
+        return self._take_bending_setups(self.loaded_dict, machine_part)
+
+    @log_function
+    def get_all_bending_setups(self):
+        """Return all bending setups from the loaded dictionary."""
+        return {
+        "df_arc": self._take_bending_setups(self.loaded_dict, "geometry_data_key_characteristics_arc"),
+        "df_lin1": self._take_bending_setups(self.loaded_dict, "geometry_data_key_characteristics_linear_1"),
+        "df_lin2": self._take_bending_setups(self.loaded_dict, "geometry_data_key_characteristics_linear_2"),
+        "df_stl_arc": self._take_bending_setups(self.loaded_dict, "geometry_data_stl_suitable_arc"),
+        "df_stl_lin1": self._take_bending_setups(self.loaded_dict, "geometry_data_stl_suitable_linear_1"),
+        "df_stl_lin2": self._take_bending_setups(self.loaded_dict, "geometry_data_stl_suitable_linear_2"),
+        "df_machine": self._take_bending_setups(self.loaded_dict, "process_parameters_loads_machine"),
+        "df_sensor": self._take_bending_setups(self.loaded_dict, "process_parameters_loads_sensor"),
+        "df_movements": self._take_bending_setups(self.loaded_dict, "process_parameters_movements"),
+        "df_bending": self._take_bending_setups(self.loaded_dict, "bending_setups"),
+    }
 
     @log_function
     def get_experiment_by_number(self, experiment_number):
         """Return a specific experiment as a dictionary given its number."""
-        return self.loaded_dict.get(f'Exp_{experiment_number}')
+        return self.loaded_dict.get(f"Exp_{experiment_number}")
 
     @log_function
     def get_experiment_keys(self):
         """Print keys of an experiment dictionary with numbering."""
-        for key in list(self.loaded_dict.get(f'Exp_{2}')):
+        for key in list(self.loaded_dict.get(f"Exp_{2}")):
             print(key)
-        
