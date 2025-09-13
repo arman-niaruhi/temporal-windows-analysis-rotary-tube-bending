b976285144a1adc36e4532bf7496710390000d65
Arman Niaruhi <arman.niaruhi@gmail.com>
2025-09-13 15:18:15 +0200

    feat: Utilize the element of Pipeline in main

--- main.py
+++ main.py
@@ -1,4 +1,50 @@
-from src.pipeline.extractor import data_extractor
+# main.py
+from src.pipeline.extractor import DataExtractor
+from src.pipeline.transformer import DataTransformer
+from pipeline.visualizer import DataVisulizer
 
-extractor = data_extractor()
 
+def main():
+    # -----------------------------
+    # 1. Extraction
+    # -----------------------------
+    extractor = DataExtractor()
+    dfs = extractor.get_all_bending_setups()
+
+    # -----------------------------
+    # 2. Transformation
+    # -----------------------------
+    transformer = DataTransformer(**dfs)
+    transformer.check_quality()
+
+    failed_indices = [1, 48, 166]
+    transformer.delete_failed_experiment(failed_experiment=failed_indices)
+
+    # Extract all tranformed sensor_data
+    df_machine_and_movement, df_sensor, df_machine, df_movements = (
+        transformer.get_process_data()
+    )
+
+    # Extract all tranformed geometry_data
+    df_arc, df_lin1, df_lin2, linear_df, all_geometry_data = (
+        transformer.get_geometry_data()
+    )
+
+    # -----------------------------
+    # 3. Visualize
+    # -----------------------------
+    vizualiser = DataVisulizer()
+    vizualiser.multi_sensor_experiment(
+        dfs=[df_machine_and_movement, df_sensor, df_arc],
+        experiment_id=123,
+        df_names=["Machine & Movement", "Sensors", "Geometry"],
+        x_axes=[
+            "index",
+            "index",
+            "Angle[degree]ORDistance[mm]",
+        ],
+    )
+
+
+if __name__ == "__main__":
+    main()
