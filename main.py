baf781641143e4303ea4e715236bea0da34fa333
Arman Niaruhi <arman.niaruhi@gmail.com>
2025-09-14 18:59:30 +0200

    feat: add new features to mains

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-# main.py
-from src.pipeline.extractor import DataExtractor
-from src.pipeline.transformer import DataTransformer
-from src.pipeline.visualizer import DataVisulizer
+from src.pipeline.preprocessing.extractor import DataExtractor
+from src.pipeline.preprocessing.transformer import DataTransformer
+from src.pipeline.report.visualizer import DataVisulizer
+from src.pipeline.ml.context_extactor import ContextExtractor
 
 
 def main():
@@ -17,25 +17,34 @@ def main():
     transformer = DataTransformer(**dfs)
     transformer.check_quality()
     transformer.delete_failed_experiment(failed_experiment=[1, 48, 166])
-    # Extract all tranformed sensor_data
-    df_machine_and_movement, df_sensor, df_machine, df_movements = (
-        transformer.get_process_data()
-    )
-    # Extract all tranformed geometry_data
-    df_arc, df_lin1, df_lin2, linear_df, all_geometry_data = (
-        transformer.get_geometry_data()
-    )
+    transformer.normalize_data()
+    
+    df_machine_and_movement, df_sensor, _, _ = transformer.get_process_data(experiment_ids=[60])
+    df_arc, _, _, _, _ = transformer.get_geometry_data(experiment_ids=[60])
 
     # -----------------------------
-    # 3. Visualize
+    # 3. Context Extraction
+    # -----------------------------
+    # context_extractor = ContextExtractor(input_df=df_machine_and_movement, target_df=df_arc)
+    # context_extractor.extract_important_window(target_column="Collapse [mm]")
+
+    # -----------------------------
+    # 4. Visualize
     # -----------------------------
     vizualiser = DataVisulizer()
-    vizualiser.multi_sensor_experiment(
-        dfs=[df_machine_and_movement, df_sensor, df_arc],
-        experiment_id=123,
-        df_names=["Machine & Movement", "Sensors", "Geometry"],
+    # vizualiser.multi_sensor_experiment(
+    #     dfs=[df_machine_and_movement, df_arc],
+    #     experiment_id=60,
+    #     df_names=["Machine & Movement", "Geometry"],
+    #     x_axes=[
+    #         "index",
+    #         "Angle[degree]ORDistance[mm]",
+    #     ],
+    # )
+    vizualiser.interactive_plot(
+        dfs=[df_machine_and_movement, df_arc],
+        df_names=["Machine & Movement", "Geometry"],
         x_axes=[
-            "index",
             "index",
             "Angle[degree]ORDistance[mm]",
         ],
