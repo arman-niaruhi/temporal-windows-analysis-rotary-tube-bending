91ba451cdf44cd04baeb2dec01cbfed480be875c
Arman Niaruhi <arman.niaruhi@gmail.com>
2025-09-19 14:09:31 +0200

    feat: differnet features added

--- main.py
+++ main.py
@@ -6,39 +6,28 @@ from src.pipeline.ml.context_extactor import ContextExtractor
 
 def main():
     # -----------------------------
-    # 3. Load Data From SQLite
+    # 1. Load Data From SQLite or update the SQLite
     # -----------------------------
-    #DataPreprocessPipeline.run()
+    # DataPreprocessPipeline.run()
     EXPERIMENT_ID = 8
-    # loader = DataLoader("data/tube_geometry.db")
-    # loaded_dfs = loader.load_data_by_experiment(EXPERIMENT_ID)
-    # df_arc = loaded_dfs["df_arc"]
-    # df_machine_and_movement = loaded_dfs["df_machine_and_movement"]
+    loader = DataLoader("data/tube_geometry.db")
+    loaded_dfs = loader.load_data_by_experiment(EXPERIMENT_ID)
+    df_arc = loaded_dfs["df_arc"]
+    df_machine_and_movement = loaded_dfs["df_machine_and_movement"]
 
     # -----------------------------
     # 2. Context Extraction
     # -----------------------------
-    #context_extractor = ContextExtractor(input_df=df_sensor_2, target_df=df_arc_2)
-    #context_extractor.extract_important_window(target_column="Collapse [mm]", num_top_windows=5, )
+    context_extractor = ContextExtractor(input_df=df_machine_and_movement, target_df=df_arc)
+    context_extractor.extract_important_window(target_column="Collapse [mm]", num_top_windows=50, )
 
     # -----------------------------
     # 3. Visualize
     # -----------------------------
     vizualiser = DataVisulizer()
-    # vizualiser.multi_sensor_experiment(
-    #     dfs=[df_machine_and_movement, df_arc],
-    #     experiment_id=EXPERIMENT_ID,
-    #     df_names=["Machine & Movement", "Geometry"],
-    #     x_axes=[
-    #         "index",
-    #         "Angle[degree]ORDistance[mm]",
-    #     ],
-    # )
-
     vizualiser.interactive_plot_streamlit()
 
 
 if __name__ == "__main__":
     main()
-    
     # streamlit run main.py 
--- src/pipeline/preprocessing/data_preprecessor.py
+++ src/pipeline/preprocessing/data_preprecessor.py
@@ -4,11 +4,45 @@ from src.pipeline.preprocessing.loader import DataLoader
 
 from src.logging.log_utils import log_function
 
-
 class DataPreprocessPipeline:
+    """
+    Pipeline for extracting, transforming, and loading bending setup and process data.
+
+    This class orchestrates the full ETL process:
+    1. Extracts bending setup and machine/process data using DataExtractor.
+    2. Transforms the data with DataTransformer (quality check, remove failed experiments, normalization, NaN handling).
+    3. Loads the final DataFrames into a SQLite database using DataLoader.
+    """
+
     @classmethod
     @log_function
     def run(cls):
+        """
+        Execute the full preprocessing pipeline: extraction, transformation, and loading.
+
+        Steps:
+        ------
+        1. Extraction:
+            - Use DataExtractor to load all bending setups into DataFrames.
+        2. Transformation:
+            - Create a DataTransformer instance with the extracted DataFrames.
+            - Check data quality and convert types.
+            - Remove failed experiments (hardcoded IDs [1, 48, 166]).
+            - Normalize numeric data.
+            - Drop all-NaN columns.
+            - Retrieve processed process and geometry DataFrames.
+            - Eliminate columns that are always constant(PRESSURE-DIE_LEFT_AXIAL_Movement_[mm], COLLET_ROTATING_Movement_[mm])
+        3. Loading:
+            - Collect selected DataFrames into a dictionary.
+            - Use DataLoader to save them to SQLite.
+            - Certain tables store the index as a column for query convenience.
+
+        Args:
+            None
+
+        Returns:
+            None: The pipeline updates and saves the DataFrames to the SQLite database.
+        """
         # -----------------------------
         # 1. Extraction
         # -----------------------------
@@ -23,11 +57,17 @@ class DataPreprocessPipeline:
         transformer.delete_failed_experiment(failed_experiment=[1, 48, 166])
         transformer.normalize_data()
         transformer.nan_handler()
-
+        transformer.save_correlation_matrices(tables=["df_machine", "df_sensor","df_movements"])
+        transformer.eliminate_column(df_name="df_movements", column_name="PRESSURE-DIE_LEFT_AXIAL_Movement_[mm]")
+        transformer.eliminate_column(df_name="df_movements", column_name="COLLET_ROTATING_Movement_[mm]")
+        
         df_machine_and_movement, df_sensor, df_machine, df_movements = transformer.get_process_data()
         df_arc, df_lin1, df_lin2, linear_df, all_geometry_data = transformer.get_geometry_data()
+        df_bending = transformer.get_bending_setup()
         
-        # Suppose you already have all your DataFrames
+        # -----------------------------
+        # 3. Loading
+        # -----------------------------
         dataframes = {
             "df_machine_and_movement": df_machine_and_movement,
             #"df_lin1": df_lin1,
@@ -38,7 +78,11 @@ class DataPreprocessPipeline:
             "df_machine": df_machine,
             "df_sensor": df_sensor,
             "df_movements": df_movements,
+            "df_bending": df_bending
         }
 
         loader = DataLoader("data/tube_geometry.db")
-        loader.save_to_sqlite(dataframes)
+        loader.store_to_sqlite(
+            dataframes,
+            store_index_tables=["df_machine", "df_sensor", "df_machine_and_movement", "df_movements"]
+        )
