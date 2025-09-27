dce714e3abca5a38d391b6b91629c46c96dedca2
Arman Niaruhi <arman.niaruhi@gmail.com>
2025-09-27 12:01:23 +0200

    feat: add new researchs

--- main.py
+++ main.py
@@ -17,13 +17,43 @@ def main():
 
     loaded_dfs = load_data()
     df_arc = loaded_dfs["df_arc"]
-    df_machine_and_movement = loaded_dfs["df_machine_and_movement"]
+    load_setup = loaded_dfs.get("df_bending", None)
+    import pandas as pd
+    from collections import defaultdict
 
+    # Columns to match
+    cols_to_match = [
+        'Pressure-die lateral position', 
+        'Pressure-die distance', 
+        'Pressure-die boost', 
+        'Mandrel position', 
+        'Mandrel retraction timing', 
+        'Collet boost', 
+        'Clamp-die lateral position'
+    ]
+
+    # Group by these columns and collect Experiment_IDs
+    grouped = load_setup.groupby(cols_to_match)['Experiment_ID'].apply(list)
+
+    # Filter only groups with more than 1 experiment (i.e., duplicates)
+    duplicates_list = [exp_list for exp_list in grouped if len(exp_list) > 1]
+
+    # Show the result
+    print(duplicates_list)
+    
+    # Take only the first experiment ID from each group
+    first_experiments = [exp_list[0] for exp_list in grouped]
+
+    # Filter the machine and movement DataFrame
+    df_machine_and_movement = loaded_dfs["df_machine_and_movement"]
+    df_machine_filtered = df_machine_and_movement[df_machine_and_movement['Experiment_ID'].isin(first_experiments)]
+    df_arc_filtered = df_arc[df_arc['Experiment_ID'].isin(first_experiments)]
+    df_machine_filtered.to_csv("features.csv")
+    df_arc_filtered.to_csv("targets.csv")
+    
     # -----------------------------
     # 2. Context Extraction
     # -----------------------------
-    # context_extractor = ContextExtractor(input_df=df_machine_and_movement, target_df=df_arc)
-    # context_extractor.extract_important_window(target_column="Collapse [mm]", num_top_windows=50, )
 
     # -----------------------------
     # 3. Visualize
